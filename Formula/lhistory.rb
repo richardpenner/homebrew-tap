264b4516272cee628c86a89802a3071540954f73
richardpenner <richardpenner@me.com>
2026-02-10 17:29:35 -0500

    lhistory 0.2.0

--- Formula/lhistory.rb
+++ Formula/lhistory.rb
@@ -2,7 +2,7 @@ class Lhistory < Formula
   desc "Cross-shell command history browser with multi-column TUI"
   homepage "https://github.com/richardpenner/lhistory"
   url "https://github.com/richardpenner/lhistory/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "778373ea9376218f7e8db51b6a5a100c21c4abf2be35e5cd2fe8ed1c701d2579"
+  sha256 "b2f105beb1159c83dd13996f4057068e8d5b2676bc506f4d4d3ca105d195d744"
   license "MIT"
 
   def install
